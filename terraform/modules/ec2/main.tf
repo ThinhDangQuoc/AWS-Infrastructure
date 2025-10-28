86de6ecdcf15223d889b47ff3f01731c345ac08e
Dang Quoc Thinh <dangquocthinh2005@gmail.com>
2025-10-28 10:16:06 -0700

    revert commit (reconfig modules)

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -6,18 +6,5 @@ resource "aws_instance" "this" {
   key_name                    = var.key_name
   vpc_security_group_ids      = [var.security_group_id]
   associate_public_ip_address = var.associate_public_ip
-  monitoring                  = true
-  ebs_optimized               = true
-  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
-
-  metadata_options {
-    http_tokens = "required"
-    http_endpoint = "enabled"
-  }
-
-  root_block_device {
-    encrypted = true
-  }
-
   tags                        = { Name = "tf-ec2-${var.role}-${count.index}" }
 }
--- terraform/modules/security_group/main.tf
+++ terraform/modules/security_group/main.tf
@@ -14,15 +14,10 @@ resource "aws_security_group" "public_ec2_sg" {
 
 
   egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow HTTPS outbound traffic"
-  }
-
-  tags = {
-    Name = "tf-public-ec2-sg"
   }
 }
 
@@ -43,14 +38,9 @@ resource "aws_security_group" "private_ec2_sg" {
 
 
   egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow outbound HTTPS only"
-  }
-
-  tags = {
-    Name = "tf-private-ec2-sg"
   }
 }
